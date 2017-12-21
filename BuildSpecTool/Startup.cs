b8b63d93ca0c082d5b85e9d0e0a2ed611f095656
Raziyeh Bazargan <R.bazargan@outlook.com>
2017-12-21 14:22:39 -0800

    Add project files.

---
+++ BuildSpecTool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BuildSpecTool.Startup))]
+namespace BuildSpecTool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
