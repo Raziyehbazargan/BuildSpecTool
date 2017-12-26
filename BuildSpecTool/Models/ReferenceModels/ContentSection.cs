64425e62b2dbb7dcbb01a2c7492f00881ab1b922
Raziyeh Bazargan <R.bazargan@outlook.com>
2017-12-26 14:21:23 -0800

    Reference Models

---
+++ BuildSpecTool/Models/ReferenceModels/ContentSection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BuildSpecTool.Models.ReferenceModels
+{
+    public class ContentSection
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ BuildSpecTool/Models/ReferenceModels/Page.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BuildSpecTool.Models.ReferenceModels
+{
+    public class Page
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; }
+        public bool IsStatic { get; set; }
+    }
+}
