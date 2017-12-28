04ab67a3770d3f91124eb4ea295bb7d46f4434d4
Raziyeh Bazargan <R.bazargan@outlook.com>
2017-12-28 08:49:17 -0800

    working on update

--- BuildSpecTool/App_Start/BundleConfig.cs
+++ BuildSpecTool/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace BuildSpecTool
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
