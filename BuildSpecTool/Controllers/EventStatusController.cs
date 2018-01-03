3169d539c0919cfed22c4d64a1d4fdba5a9b39d6
Raziyeh Bazargan <R.bazargan@outlook.com>
2018-01-02 19:50:31 -0800

    Component View

---
+++ BuildSpecTool/Controllers/EventStatusController.cs
@@ -0,0 +1,24 @@
+﻿using BuildSpecTool.Models.ReferenceModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuildSpecTool.Controllers
+{
+    public class EventStatusController : Controller
+    {
+        // GET: EventStatus
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Save(EventStatus details)
+        {
+
+            return View();
+        }
+    }
+}
